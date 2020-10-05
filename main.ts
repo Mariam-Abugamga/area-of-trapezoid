1416fcb62f0a38951f8ebeca8b60746283ced146
Mariam-Abugamga <71400493+Mariam-Abugamga@users.noreply.github.com>
2020-10-05 15:44:08 -0400

    added a comment and made a game to find the area of a trapezoid

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
+// Created by: Mariam Abugamga
+// 
+// Created on: Oct, 5, 2020
+// 
+// This program finds the area of a trapezoid
 game.splash("Let's calculate the area of a Trapezoid. ")
+let top = game.askForNumber("Enter the top of the trapezoid")
+let bottom = game.askForNumber("Enter the bottom of the trapezoid")
+let height = game.askForNumber("Enter the height of the trapezoid")
+let area = (top + bottom) * (height / 2)
+game.splash("The area of the Trapezoid" + top + "cm by" + (top + bottom) + "cm is" + height + "cm is" + convertToText(area) + "cm2")
